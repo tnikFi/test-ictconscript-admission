fd698ab863358414c7141e827b884030a4482a36
tnikFi <95296580+tnikFi@users.noreply.github.com>
2025-07-20 00:16:14 +0300

    Add test for GetLogbookEntryQuery

---
+++ tests/Integration.Tests/Queries/GetLogbookEntryQueryTests.cs
@@ -0,0 +1,31 @@
+﻿using Application.Queries;
+using Domain.Entities;
+
+namespace Integration.Tests.Queries;
+
+public class GetLogbookEntryQueryTests : TestBase
+{
+    [Fact]
+    public async Task TestGetLogbookEntryQuery()
+    {
+        await Context.LogbookEntries.AddRangeAsync(new LogbookEntry
+        {
+            Title = "test",
+            Body = "testing",
+            Timestamp = DateTimeOffset.Now
+        }, new LogbookEntry
+        {
+            Title = "test 2",
+            Body = "testing again",
+            Timestamp = DateTimeOffset.Now
+        });
+        await Context.SaveChangesAsync();
+
+        var result =
+            await Mediator.SendAsync<GetLogbookEntriesQuery, IEnumerable<LogbookEntry>>(new GetLogbookEntriesQuery());
+        var entries = result.ToList();
+        
+        Assert.NotEmpty(entries);
+        Assert.Equal(2, entries.Count);
+    }
+}
---
+++ tests/Integration.Tests/TestBase.cs
@@ -0,0 +1,42 @@
+﻿using Application;
+using Common.Interfaces;
+using Cortex.Mediator;
+using Cortex.Mediator.DependencyInjection;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Integration.Tests;
+
+public class TestBase : IAsyncLifetime
+{
+    private IServiceProvider? _serviceProvider;
+    private IServiceProvider ServiceProvider => _serviceProvider ?? throw new InvalidOperationException();
+    protected IMediator Mediator => ServiceProvider.GetRequiredService<IMediator>();
+    protected ApplicationDbContext Context => ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    public Task InitializeAsync()
+    {
+        var services = new ServiceCollection();
+        var config = new ConfigurationBuilder()
+            .Build();
+        services.AddCortexMediator(
+            config,
+            [typeof(IApplicationMarker)]
+        );
+        services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(opts =>
+        {
+            opts.UseInMemoryDatabase("TestDb");
+            opts.EnableSensitiveDataLogging();
+        });
+        _serviceProvider = services.BuildServiceProvider();
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        _serviceProvider = null!;
+        return Task.CompletedTask;
+    }
+}
