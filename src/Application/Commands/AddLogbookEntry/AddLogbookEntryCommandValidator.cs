03e159582eaf993c860a4928500aadd1fa5ce026
tnikFi <95296580+tnikFi@users.noreply.github.com>
2025-07-24 18:31:05 +0300

    Fix validation error messages being shown incorrectly

--- src/Application/Commands/AddLogbookEntry/AddLogbookEntryCommandValidator.cs
+++ src/Application/Commands/AddLogbookEntry/AddLogbookEntryCommandValidator.cs
@@ -13,14 +13,19 @@ public class AddLogbookEntryCommandValidator : AbstractValidator<AddLogbookEntry
         // Title is required and should be at most 120 chars.
         RuleFor(c => c.Title)
             .NotEmpty()
-            .WithMessage("Title is required")
-            .Length(1, 120)
+            .WithMessage("Title is required");
+            
+        RuleFor(c => c.Title)
+            .MaximumLength(120)
             .WithMessage("Title is too long");
 
         // Body can be an empty string, but shouldn't be null.
         // We'll use a maximum length of 2000 here to limit entries to a reasonable size
-        RuleFor(c => c.Body).NotNull()
-            .WithMessage("Invalid body")
+        RuleFor(c => c.Body)
+            .NotNull()
+            .WithMessage("Invalid body");
+            
+        RuleFor(c => c.Body)
             .MaximumLength(2000)
             .WithMessage("Entry body is too long");
 
@@ -29,13 +34,17 @@ public class AddLogbookEntryCommandValidator : AbstractValidator<AddLogbookEntry
         {
             RuleFor(c => c.Lat)
                 .NotNull()
-                .WithMessage("Latitude is required when longitude is defined")
+                .WithMessage("Latitude is required when longitude is defined");
+                
+            RuleFor(c => c.Lat)
                 .InclusiveBetween(-90, 90)
                 .WithMessage("Latitude must be between -90 and 90");
 
             RuleFor(c => c.Lon)
                 .NotNull()
-                .WithMessage("Longitude is required when latitude is defined")
+                .WithMessage("Longitude is required when latitude is defined");
+                
+            RuleFor(c => c.Lon)
                 .InclusiveBetween(-180, 180)
                 .WithMessage("Longitude must be between -180 and 180");
         });
